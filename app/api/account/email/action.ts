95ea30577f2f3547ac7766dd02642d5f8a5d8828
Aceboy76 <harthpalaras62@gmail.com>
2024-12-22 17:42:31 +0800

    as

--- app/api/account/email/action.ts
+++ app/api/account/email/action.ts
@@ -18,7 +18,18 @@ const smtpConfig = {
     }
 };
 
-export const sendVerificationEmail = async (email: string, token:string) => {
+
+const brandColor = "#346df1"
+const color = {
+    background: "#f9f9f9",
+    text: "#444",
+    mainBackground: "#fff",
+    buttonBackground: brandColor,
+    buttonBorder: brandColor,
+    buttonText: "#fff",
+}
+
+export const sendVerificationEmail = async (email: string, token: string) => {
 
     try {
         var transporter = nodemailer.createTransport(smtpConfig);
@@ -30,7 +41,35 @@ export const sendVerificationEmail = async (email: string, token:string) => {
             from: process.env.EMAIL_USER,
             to: email,
             subject: 'Email Verification',
-            html: `<p>Please verify your email by clicking on the link provided. This will confirm your email address and complete the registration process. You can verify your email by clicking on the 'Verify Email' link in the message we sent you: <a href="${verificationUrl}">Verify Email</a></p>`,
+            html: `<body style="background: ${color.background};">
+  <table width="100%" border="0" cellspacing="20" cellpadding="0"
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center"
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        Sign in to <strong>E Commerce App</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${verificationUrl}"
+                target="_blank"
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
+                in</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center"
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        If you did not request this email you can safely ignore it.
+      </td>
+    </tr>
+  </table>
+</body>`,
         });
     } catch (error) {
         console.error(error);
